d09e7be12f376a0cb97a6c33cf5a237af0782d3e
Edward Stewart <eddstewart1@hotmail.co.uk>
2023-12-31 00:15:57 +0000

    add js functionality to add listing to database & page as soon as created

--- client/app.js
+++ client/app.js
@@ -1,13 +1,22 @@
 const newListingForm = document.getElementById("newListingForm");
 const createNewListingBtn = document.getElementById("createNewListingBtn");
+const listingArea = document.getElementById("listingArea");
 
+// The below function gets all of the database entries (game listings) and displays them on the page when it is loaded.
 async function getListings() {
-    const response = await fetch("https://localhost:8080/marketplacelistings");
+    const response = await fetch("http://localhost:8080/marketplacelistings");
     const listings = await response.json();
-};
+    listings.forEach(function(listing) {
+        const { title, price, quality, description } = listing;    
+        const p = document.createElement("p");
+        p.textContent = `${title}, ${price}, ${quality}, ${description}`;    
+        listingArea.appendChild(p);
+    });
+}
 
 getListings();
 
+// This is the event listener for creating a new listing from the main marketplace page //
 createNewListingBtn.addEventListener("click", function() {
     newListingForm.style.opacity = "1";
     newListingForm.style.pointerEvents = "all";
@@ -22,9 +31,18 @@ async function createNewListing() {
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(formValues),        
+        body: JSON.stringify(formValues),              
     })
     newListingForm.reset();
+    const title = formValues.title;
+    const price = formValues.price;
+    const quality = formValues.quality;
+    const description = formValues.description;
+    const p = document.createElement("p");
+    p.textContent = `${title}, ${price}, ${quality}, ${description}`; 
+    listingArea.appendChild(p);
+
+
 };
 
 // Event listener for create button on form //
@@ -33,4 +51,25 @@ newListingForm.addEventListener("submit", function(event) {
     createNewListing();
     newListingForm.style.opacity = "0";
     newListingForm.style.pointerEvents = "none";
-})
+});
+
+
+
+
+
+
+
+// for (let i = 0; i < listings.length; i++) {
+//     const title = listings[i].title;
+//     const price = listings[i].price;
+//     const quality = listings[i].quality;
+//     const description = listings[i].description;
+//     const p = document.createElement("p");
+//     p.textContent = `${title}`;
+//     p.textContent = `${price}`;
+//     p.textContent = `${quality}`;
+//     p.textContent = `${description}`;
+//     const listingArea = document.getElementById("listingArea");
+//     listingArea.appendChild(p);
+// }
+// };
